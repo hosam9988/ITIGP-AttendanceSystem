9f86a66ff6c6c2d2a94440042d77caeb3441d8a3
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-08 21:06:16 +0200

    add repository to to the skeltone

---
+++ Repository/AppRepository.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository
+{
+    public class AppRepository<T>:IAppRepository<T> where T : class
+    {
+        protected readonly AttendContext _context;
+        public AppRepository(AttendContext context)
+        {
+            _context = context;
+        }
+        
+        public void Create(T entity) => _context.Set<T>().Add(entity);
+
+        public void Delete(T entity) => _context.Set<T>().Remove(entity);
+
+        public void Update(T entity) => _context.Set<T>().Update(entity);
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ? _context.Set<T>().AsNoTracking() : _context.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ? _context.Set<T>().Where(expression).AsNoTracking() : _context.Set<T>().Where(expression);
+    }
+}
