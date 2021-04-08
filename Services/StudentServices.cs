019f01867262eb6f94b189c619848c3d64436774
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-08 21:07:31 +0200

    add services to the skeletone

---
+++ Services/StudentServices.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class StudentServices
+    {
+        private readonly IAppRepository<Student> _appRepository; //=>use model from student model
+        public StudentServices(IAppRepository<Student> appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        public void create(Student student)
+        {
+            _appRepository.Create(student);
+        }
+    }
+}
