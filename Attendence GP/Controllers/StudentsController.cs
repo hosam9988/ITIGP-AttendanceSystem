4f2908f45574e7c291b9675814730d23080f6fdc
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-12 02:18:25 +0200

    updated StudentsController and Added TrackActionsController

--- Attendence GP/Controllers/StudentsController.cs
+++ Attendence GP/Controllers/StudentsController.cs
@@ -12,14 +12,12 @@ using System.Threading.Tasks;
 
 namespace Attendence_GP.Controllers
 {
-    [Route("track/{trackActionId}/[controller]/")]
+    [Route("trackAction/{trackActionId}/[controller]/")]
     [ApiController]
-
     public class StudentsController : ControllerBase
     {
         private readonly IStudentServices _studentServices;
         
-
         public StudentsController(IStudentServices studentServices)
         {
             _studentServices = studentServices;
@@ -32,16 +30,16 @@ namespace Attendence_GP.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetStudentsForTrack(int trackActionId, bool trackChanges)
+        public async Task<IActionResult> GetStudentsForTrack(int trackActionId)
         {
-            var students = await _studentServices.GetStudentsForTrack(trackActionId, trackChanges: false);
+            var students = await _studentServices.GetStudentsForTrack(trackActionId);
             return Ok(students);
         }
         [HttpGet("{studentId}")]
-        public async Task<IActionResult> GetStudentPerId(int trackActionId, int studentId, bool trackChanges)
+        public async Task<IActionResult> GetStudentPerId(int trackActionId, int studentId)
         {
             
-            var student = await _studentServices.GetStudent(trackActionId, studentId, false);
+            var student = await _studentServices.GetStudent(trackActionId, studentId);
             return Ok(student);
         }
 
@@ -50,6 +48,15 @@ namespace Attendence_GP.Controllers
         public async Task<IActionResult> UpdateStudentForTrack(int trackActionId, int studentId, [FromBody] UpdateStudentVM student)
         {
             await _studentServices.Update(trackActionId, studentId, student);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{studentId}")]
+
+        public async Task<IActionResult> DeleteStudentForTrack(int trackActionId, int studentId)
+        {
+            await _studentServices.Delete(trackActionId, studentId);
             return NoContent();
         }
     }
---
+++ Attendence GP/Controllers/TrackActionsController.cs
@@ -0,0 +1,61 @@
+﻿using Contracts.ServicesContracts;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attendence_GP.Controllers
+{
+    [Route("track/{trackId}/[controller]")]
+    [ApiController]
+    public class TrackActionsController : ControllerBase
+    {
+
+        private readonly ITrackActionServices _trackActionServices;
+
+        public TrackActionsController(ITrackActionServices trackActionServices)
+        {
+            _trackActionServices = trackActionServices;
+        }
+
+        [HttpPost]
+        public async Task AddStudent(int trackId, [FromBody] TrackAction trackAction)
+        {
+            await _trackActionServices.Create(trackId, trackAction);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTrackActionsForTrack(int trackId)
+        {
+            var trackActions = await _trackActionServices.GetTrackActionsForTrack(trackId);
+            return Ok(trackActions);
+        }
+        [HttpGet("{trackActionId}")]
+        public async Task<IActionResult> GetTrackActionsPerId(int trackId, int trackActionId)
+        {
+
+            var trackAction = await _trackActionServices.GetTrackAction(trackId, trackActionId);
+            return Ok(trackAction);
+        }
+
+        [HttpPut("{trackActionId}")]
+
+        public async Task<IActionResult> UpdateStudentForTrack(int trackId, int trackActionId, [FromBody] TrackAction trackAction)
+        {
+            await _trackActionServices.Update(trackId, trackActionId, trackAction);
+            return NoContent();
+        }
+
+
+        [HttpDelete("{trackActionId}")]
+
+        public async Task<IActionResult> DeleteStudentForTrack(int trackId, int trackActionId)
+        {
+            await _trackActionServices.Delete(trackId, trackActionId);
+            return NoContent();
+        }
+    }
+}
