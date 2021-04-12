4fcd12a19c0426d253ef407a72da8d520818c772
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-12 02:23:07 +0200

    update API project

--- Attendence GP/Startup.cs
+++ Attendence GP/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Services.Mapper;
 
 namespace Attendence_GP
 {
@@ -21,19 +21,14 @@ namespace Attendence_GP
             _configuration = configuration;
         }
         public void ConfigureServices(IServiceCollection services)
-        {
-            
+        { 
             services.ConfigureDbContext(_configuration);
             services.ConfigureCors();
             services.ConfigureRepositoryManager();
-            services.AddAutoMapper(typeof(Startup));
+            services.AddAutoMapper(typeof(MappingProfile));
             services.ConfigureStudentService();
-            services.AddControllers(config =>
-            {
-                config.ReturnHttpNotAcceptable = true;
-            });//.AddXmlDataContractSerializerFormatters();
-            //AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
-
+            services.ConfigureTrackActionsService();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
