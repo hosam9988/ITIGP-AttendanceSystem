8662f287ff9b79884f59bcc912224fd00fa74fb7
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-13 02:44:08 +0200

    add track Controller

---
+++ Attendence GP/Controllers/TrackController.cs
@@ -0,0 +1,29 @@
+﻿using Contracts.ServicesContracts;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attendence_GP.Controllers
+{
+    [Route("programs/{programId}/[controller]")]
+    [ApiController]
+    public class TrackController : ControllerBase
+    {
+        private readonly ITrackServices _trackServices;
+        public TrackController(ITrackServices trackServices)
+        {
+            _trackServices = trackServices;
+        }
+
+        [HttpPost]
+        public async Task AddTrack(int programId, [FromBody] Track track)
+        {
+            await _trackServices.Create(programId, track);
+        }
+
+    }
+}
