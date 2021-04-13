c63fd3325f485c8ed085d3d4c43206a3858f86be
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-13 02:45:20 +0200

    add track services

---
+++ Contracts/ServicesContracts/ITrackServics.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.ServicesContracts
+{
+    public interface ITrackServices
+    {
+        Task Create(int ProgramId, Track track);
+        Task Update(int programId, int id, Track track);
+        Task Delete(int programId, int id);
+        Task<Track> GetTrack(int programId, int id);
+        Task<List<Track>> GetTracksForProgram(int programId);
+    }
+}
