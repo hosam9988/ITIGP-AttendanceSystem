932cf7ac6f1cdaad9f711cf23fd05e2284f02500
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-15 16:57:21 +0200

    add atendance Repository

---
+++ Contracts/RepositoryContracts/IAttendanceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.RepositoryContracts
+{
+    public interface IAttendanceRepository
+    {
+        Task<List<Attendance>> GetAttendances(int trackActionId, bool trackChanges);
+        Task<Attendance> GetAttendanceForTrack(int trackActionId, DateTime date, bool trackChanges);
+        void CreateTrackAttendance(int trackActionId, Attendance attendance);
+        void DeleteAttendanc(Attendance attendance);
+        void UpdateAttendanc(Attendance attendance);
+    }
+}
