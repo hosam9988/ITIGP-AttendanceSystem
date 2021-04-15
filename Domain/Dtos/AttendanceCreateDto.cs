9208b744e00a3c6502c3c090f3aed3f7e14e56b7
Taha Elfakharany <taha.ashri96@outlook.com>
2021-04-15 16:57:36 +0200

    attendnace model

---
+++ Domain/Dtos/AttendanceCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+    public class AttendanceCreateDto
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan? AttendAt { get; set; }
+        public TimeSpan? LeaveAt { get; set; }
+    }
+}
