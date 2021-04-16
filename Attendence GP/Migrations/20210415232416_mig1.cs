69811d6f8e60303d667ce78c6f146c4647bf6f63
hosam9988 <hosamkhaled1001@gmail.com>
2021-04-16 05:40:06 +0200

    migration used to update database

---
+++ Attendence GP/Migrations/20210415232416_mig1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Attendence_GP.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Noe",
+                table: "Permission",
+                newName: "Note");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Note",
+                table: "Permission",
+                newName: "Noe");
+        }
+    }
+}
